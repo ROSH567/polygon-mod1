f1069bceb2d3cbf9e11b280a766af01c6d971759
ROSH567 <97090887+ROSH567@users.noreply.github.com>
2023-07-29 15:51:10 +0530

    Update approve.js

--- scripts/approve.js
+++ scripts/approve.js
@@ -2,19 +2,26 @@ const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
 const tokenContractJSON = require("../artifacts/contracts/BetaToken.sol/MyNFT.json");
 
-const tokenAddress = "0x4412E53CC568847e010e58303f8Ce836b781B370"; 
+const tokenAddress = "0x0A3aB21cfa850a2c519f3061225Ae2601e379aDc"; 
 const tokenABI = tokenContractJSON.abi;
 const fxERC721RootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
-const walletAddress = "0xbEefaF34CFB4FF3Cae84C6DA56A6D53B5D40A29a"; 
+const walletAddress = "0xbEEfeA95ec64eAdfdb46733fA435fe8C72E73A61"; 
 const tokenIds=[0,1,2,3,4];
 async function main() {
 
     const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+    const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC721RootAddress);
 
    for(let i=0;i<tokenIds.length;i++)
     {
     const approve = await tokenContract.approve(fxERC721RootAddress, tokenIds[i]);
     await approve.wait();
+    console.log("APPROVED NFT "+ tokenIds[i]+1);
+    const depositTx = await fxContract.deposit(tokenAddress,walletAddress,tokenIds[i],"0x6556");
+    await depositTx.wait();
+    console.log("Deposited NFT "+ tokenIds[i]+1);
+
+
     }
     
   }
